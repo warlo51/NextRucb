b4e0d1c80d4231511277b14b806b0db378a77cc4
mansuy <mmansuy@norauto.com>
2022-07-15 10:37:01 +0200

    test

--- pages/partenaires/info.tsx
+++ pages/partenaires/info.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import {Layout} from "../../components/Layout";
 export default function info() {
   const itemData = [
-    "../CD51.png",
+    "../comite51.png",
     "../LogoVilledeReims.jpg",
     "../liguegdestbasket.png",
     "../ffbb.png",
