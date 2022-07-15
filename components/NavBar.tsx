7188637803aefd9e987ce75995a7b3b014eb41e3
mansuy <mmansuy@norauto.com>
2022-07-15 22:20:26 +0200

    css

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -21,12 +21,12 @@ export default function NavBar(props: any) {
     if(props.colorText === "white"){
       return (
         <Navbar bg="" expand="lg" >
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle id="buttonToggle" aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav id="nav" className={fix ? "NavBar fixed" : "NavBar"}>
-              <Nav.Link href="/" style={{color:`${props.colorText}`}}>Accueil</Nav.Link>
-              <Nav.Link href="/formation" style={{color:`${props.colorText}`}}>Formation</Nav.Link>
-              <Nav.Link href="" ><img alt="logoRUCB" src="../logoruc.png"/></Nav.Link>
+              <Nav.Link id="link" href="/" style={{color:`${props.colorText}`}}>Accueil</Nav.Link>
+              <Nav.Link id="link" href="/formation" style={{color:`${props.colorText}`}}>Formation</Nav.Link>
+              <Nav.Link id="link" href="" ><img alt="logoRUCB" src="../logoruc.png"/></Nav.Link>
               <NavDropdown title="Qui Sommes Nous" id="basic-nav-dropdown" >
                 <NavDropdown.Item id="linkDropdown" href="/qui/historique" >Histoire Du RUC</NavDropdown.Item>
                 <NavDropdown.Item id="linkDropdown" href="/qui/comite">Comité Directeur</NavDropdown.Item>
@@ -44,7 +44,7 @@ export default function NavBar(props: any) {
     }else{
       return (
         <Navbar bg="" expand="lg" >
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" id="buttonToggle"/>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav id="nav" className={fix ? "NavBar fixed" : "NavBar"}>
               <Nav.Link href="/" style={{color:`${props.colorText}`}}>Accueil</Nav.Link>
