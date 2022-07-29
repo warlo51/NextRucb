f0b3244c5b1b4517a786d1ef8117d998527f4e6a
mansuy <mmansuy@norauto.com>
2022-07-29 21:07:40 +0200

    consolelog

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -4,7 +4,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  
   const url = `https://${process.env.AUTH0_DOMAIN}/authorize?client_id=${process.env.AUTH0_CLIENTID}&response_type=code&redirect_uri=${process.env.AUTH0_REDIRECT_URI}&audience=${process.env.AUTH0_AUDIENCE}&scope=${process.env.AUTH0_SCOPE}&state=`;
+  console.log(url)
   res.setHeader("Content-Type", "application/json");
   res.redirect(303, url);
 }
