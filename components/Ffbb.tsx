65b585b5d024ce0895ec6cba651b3ff8835736c0
mansuy <mmansuy@norauto.com>
2022-07-30 02:05:15 +0200

    dossier sponsor et licence

--- components/Ffbb.tsx
+++ components/Ffbb.tsx
@@ -4,7 +4,8 @@ export default function Ffbb() {
     return (
         <div className="divFfbb">
         <Button id="badge">Actu FFBB</Button>
-        <iframe src="https://feed.mikle.com/widget/v2/155831/?preloader-text=Loading" height="558px" width="100%" className="fw-iframe" scrolling="no" frameBorder="0"></iframe>
+        <iframe height="558px" width="100%"src="https://rss.app/embed/v1/wall/50TfAPh36gQZ26Xk" frameBorder="0"></iframe>
+        
         </div>
     );
 }
