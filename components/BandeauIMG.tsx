91ce44689043db40fa3f7fdc90182337239d3e3a
maxime mansuy <mmansuy@norauto.com>
2022-09-02 17:06:20 +0200

    add google analytic

--- components/BandeauIMG.tsx
+++ components/BandeauIMG.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import useAnalyticsEventTracker from "../src/useAnalyticsEventTracker";
 
 export default function BandeauIMG() {
   const [articlesJSON, setArticlesJSON] = React.useState<any>([]);
@@ -23,11 +24,12 @@ export default function BandeauIMG() {
   return(
   <Carousel autoPlay  infiniteLoop={true} interval={4000} className="Bandeau">
     {articlesJSON.map((article: any, index:number) =>{
+        const gaEventTracker = useAnalyticsEventTracker(article.titre);
               return (
                 <div key={index}>
                   <img src={article.image} />
                   <p className="title" style={{color:`${article.colorText}`}}>{article.titre}</p>
-                  <Button id="buttonSlide" style={{color:`${article.colorTextButton}`,backgroundColor:`${article.colorBackgroundButton}`}}href={article.lienPage}>Cliquez ici</Button>
+                  <Button id="buttonSlide" onClick={()=>gaEventTracker('click')} style={{color:`${article.colorTextButton}`,backgroundColor:`${article.colorBackgroundButton}`}}href={article.lienPage}>Cliquez ici</Button>
                 </div>
               );
             })}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,16 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import ReactGA from 'react-ga';
 
+ 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  ReactGA.initialize(`${process.env.TRACKING_ID}`);
   return <div>
       <Head>
         <title>RUCB Basket</title>
+        
         <meta name="description" content="Site web du RUCB, club de basket REIMS"/>
         <link rel="icon" type="image/png" sizes="32x32" href="../logoruc.ico"></link>
       </Head>
