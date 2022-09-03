70d60de0439d2dfde27d563912d4a3652a9287b1
maxime mansuy <mmansuy@norauto.com>
2022-09-03 21:56:18 +0200

    test analytic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import Script from 'next/script'
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
  
 
@@ -11,23 +11,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>RUCB Basket</title>
         <meta name="description" content="Site web du RUCB, club de basket REIMS"/>
         <link rel="icon" type="image/png" sizes="32x32" href="../logoruc.ico"></link>
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-
-        <Script id="google-analytics" strategy="lazyOnload">
-          {`
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                  });
-                      `}
-        </Script>
-
       </Head>
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </div>
 }
