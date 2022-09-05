40f9e10d382dd2494d150675c8adece70bb3f2bd
maxime mansuy <mmansuy@norauto.com>
2022-09-05 16:51:38 +0200

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,22 +11,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>RUCB Basket</title>
         <meta name="description" content="Site web du RUCB, club de basket REIMS"/>
         <link rel="icon" type="image/png" sizes="32x32" href="../logoruc.ico"></link>
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-
-        <Script id="google-analytics" strategy="lazyOnload">
-          {`
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                  });
-                      `}
-        </Script>
-
       </Head>
       <Component {...pageProps} />
     </div>
