00c77906ed83f29eba28d8178d5656ae7f7023a7
maxime mansuy <mmansuy@norauto.com>
2022-09-06 00:42:43 +0200

    revert 48c1c2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "/app";
+import { getAnalytics } from "/analytics";
  
 
 function MyApp({ Component, pageProps }: AppProps) {
   
-  const firebaseConfig = {
+  const Config = {
     apiKey: "AIzaSyC2xn3enhRvBpbx5Wd5EZ-Vp0XQok3DcLI",
-    authDomain: "rucb-basket-firebase.firebaseapp.com",
-    projectId: "rucb-basket-firebase",
-    storageBucket: "rucb-basket-firebase.appspot.com",
+    authDomain: "rucb-basket-.app.com",
+    projectId: "rucb-basket-",
+    storageBucket: "rucb-basket-.appspot.com",
     messagingSenderId: "307979119515",
     appId: "1:307979119515:web:e1b1ca2a20433176a8cf3e",
     measurementId: "G-2C504YHSQB"
   };
-  const app = initializeApp(firebaseConfig);
+  const app = initializeApp(Config);
   const analytics = getAnalytics(app);
   return <div>
       <Head>
