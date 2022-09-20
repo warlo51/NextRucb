f94ae199f66634fb971ce9bdcb638199e700d81a
maxime mansuy <mmansuy@norauto.com>
2022-09-20 10:20:21 +0200

    maj equipe

--- components/Equipes.tsx
+++ components/Equipes.tsx
@@ -1,20 +1,40 @@
-import { Button } from "react-bootstrap";
+import {Button, Table} from "react-bootstrap";
 
 export default function Equipes() {
     return (
         <div className="divEquipe">
         <Button id="badge">Nos Equipes</Button>
-        <ul id="ulEquipe">
-            <li>Sénior 1 : Pre région</li>
-            <li>Senior 2 : Département</li>
-            <li>U17 : Département </li>
-            <li>U15 : Département</li>
-            <li>U13 : Département</li>
-            <li>U11 : Département</li>
-            <li>U11 fille : Département</li>
-            <li>U9 : Ecole de mini basket</li>
-            <li>U7 : Ecole de mini basket</li>
-        </ul>
+            <Table striped bordered hover size="sm">
+                <thead>
+                <tr>
+                    <th>Ecole de basket</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr><td>U7</td></tr>
+                <tr><td>U9</td></tr>
+                <tr><td>U11 (2 équipes)</td></tr>
+                </tbody>
+                <thead>
+                <tr>
+                    <th>Académie de basket</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr><td>U11</td></tr>
+                <tr><td>U15</td></tr>
+                </tbody>
+                <thead>
+                <tr>
+                    <th>Secteur seniors</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr><td>U17</td></tr>
+                <tr><td>D2</td></tr>
+                <tr><td>PR1</td></tr>
+                </tbody>
+            </Table>
         </div>
     );
 }
